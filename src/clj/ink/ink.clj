ad32453cdd07f4e9f40c59726807a7c3eea23cb1
DR <daniel3735928559@gmail.com>
2017-10-09 14:22:00 +0000

    state machine builder macro

--- src/clj/ink/ink.clj
+++ src/clj/ink/ink.clj
@@ -1,17 +1,47 @@
 (require '[clojure.string :as str])
 (use 'clojure.pprint)
+;;(use 'ink.fsm)
 
-(defn get-parsed []
+(defn sm-step [dfa] (fn [state] (fn [input] (try (or (((dfa :transition) state) input) state) (catch Exception e state)))))
+
+(defn read-parsed []
   (apply hash-map
          (interleave '(:verb :object) (str/split (str/trim (read-line)) #" +" 2))))
 
-(defn get-event [model]
+(defn get-targets [model event object]
+  (let [in-scope ((event :scope) model)
+        targets (if object (in-scope object) in-scope)]
+    targets))
+
+(defn read-event [model]
   (let [action (get-parsed)
         verb (action :verb)
         event ((model :events) verb)
-        in-scope ((event :scope) model)
-        targets (if (contains? action :object) (in-scope (action :object)) in-scope)]
-    {:event (event :transition) :targets targets}))
+        targets (get-targets model event (action :object))]
+    (assoc event :targets targets)))
+
+(defn do-transition [sm transition]
+  (let [result (((sm-step sm) (sm :current)) transition)
+        new-state (result :target)
+        new-sm (assoc sm :current new-state)
+        new-events (result :events)]
+    [new-sm new-events]))
+  
+(defn do-event [model event obj]
+  (let [sm (obj :state-machine)
+        [new-sm new-events] (do-transition sm (event :transition))
+        new-obj (assoc obj :state-machine new-sm)]
+    [new-obj new-events]))
+
+(defn apply-events [model events]
+    (let [results (for [e events] (map (partial do-event model e) (e :targets)))
+          [new-objs lists-of-events] (apply mapv vector results)
+          new-events (concat lists-of-events)
+          new-model (apply (partial assoc (model :objects)) (interleave new-objs))]
+      (if (= (count new-events) 0) new-model (recur new-model new-events))))
+
+
+
 
 (defn scope-all [model] (model :objects))
 
@@ -21,29 +51,79 @@
 (def evt2 {:transition :full
            :scope scope-all})
 
-(def hotdog
-  {
-   :descriptor "dis knot paystaree"
-   :inventory []
-   :state-machine {
-        :states {:ready "looks bad" :halfeaten "regrets"}
-        :transitions {
-                      :ready {:eat {:target :halfeaten :events [evt2]}}
-                      }
-        :current :awake
-        }})
+(def evt3 {:transition :regrets-button
+           :scope scope-all})
 
-(def player-obj
-  {
-   :descriptor "u ar pastrie shef"
-   :inventory [hotdog]
-   :state-machine {
-        :states {:awake "You're awake!" :asleep "zzz"}
-        :transitions {
-                      :awake {:full {:target :asleep}}
-                      :asleep {:alarm {:target :awake}}}
-        :current :awake
-                   }})
+
+
+;; (defo hotdog []
+;;   (s* ready "looks bad")
+;;   (s halfeaten "regrets")
+;;   (t eat ready halfeaten (full)))
+  
+;; obj hotdog
+;; state* ready "looks bad"
+;; state halfeaten "regrets"
+;; trans eat ready > halfeaten (full)
+
+(defmacro mhm [a] `(apply hash-map (flatten ~a)))
+
+(defmacro findfirst [f l] `(first (filter ~f ~l)))
+
+(defmacro defo [n inventory & args]
+  (let [start (keyword ((findfirst #(= (name (first %)) "s*") args) 1))
+        state-args (filter #(contains? #{"s" "s*"} (name (first %))) args)
+        states (mhm (map #(list (keyword (% 1)) (% 2)) state-args))
+        transition-args (filter (fn [a] (= (name (first a)) "t")) args)
+        transitions (mhm (for [s (keys states)]
+                           (list s (mhm (map #(list (keyword (% 1)) {:target (keyword (% 4)) :events (vec (map keyword (% 5)))})
+                                             (filter #(= (name s) (name (% 2))) transition-args))))))]
+    ('def n {
+              :descriptor (str (name n))
+              :inventory inventory
+              :state-machine
+              {
+               :state states
+               :transition transitions
+               :current start
+               }})))
+
+(defo hotdog []
+  [s* ready "dis knot paystaree"]
+  [s halfeaten "regrets"]
+  [t eat ready > halfeaten [full]])
+
+(defo player-obj [hotdog]
+  [s* awake "u ar pastrie shef"]
+  [s asleep "zzz"]
+  [t full awake > asleep []]
+  [t alarm asleep > awake []])
+
+[hotdog player-obj]
+
+;; (def hotdog
+;;   {
+;;    :descriptor "hotdog"
+;;    :inventory []
+;;    :state-machine {
+;;         :state {:ready "dis knot paystaree" :halfeaten "regrets"}
+;;         :transition {
+;;                       :ready {:eat {:target :halfeaten :events [evt2]}}
+;;                       }
+;;         :current :ready
+;;         }})
+
+;; (def player-obj
+;;   {
+;;    :descriptor "u ar pastrie shef"
+;;    :inventory [hotdog]
+;;    :state-machine {
+;;         :state {:awake "You're awake!" :asleep "zzz"}
+;;         :transition {
+;;                       :awake {:full {:target :asleep}}
+;;                       :asleep {:alarm {:target :awake}}}
+;;         :current :awake
+;;                    }})
 
 (def das-model
   {
@@ -61,8 +141,10 @@
 ;;       (recur next-model))))
 
 (defn run [model]
+  (let [event (read-event model)
+        new-model (do-event model event (event :targets))]
     (do
-      (pprint (get-event model))
-      (recur das-model)))
+      (pprint new-model)
+      (recur new-model))))
 
 (run das-model)
---
+++ src/clj/ink/sm-build.clj
@@ -0,0 +1,34 @@
+
+(defmacro defo [n inventory & args]
+  (let [state-args (map rest (filter #(contains? #{"s" "s*"} (name (first %))) args))
+        states (apply hash-map (flatten (map #(list (keyword (first %)) (second %)) state-args)))
+        start (keyword (second (first (filter #(= (name (first %)) "s*") args))))
+        transition-args (map vec (map rest (filter (fn [a] (= (name (first a)) "t")) args)))
+        transitions (apply hash-map (flatten (for [s (keys states)]
+                      [s
+                       (apply
+                        hash-map
+                        (flatten
+                         (map #(list (keyword (get % 0))
+                                     {:target (keyword (get % 3))
+                                      :events (vec (map keyword (get % 4)))})
+                              (filter
+                               #(= (name s) (name (get % 1)))
+                               transition-args))))])))]
+    `(def ~n {
+             :descriptor ~(str (name n))
+             :inventory ~inventory
+             :state-machine
+             {
+              :state ~states
+              :transition ~transitions
+              :current ~start
+              }})))
+
+
+(macroexpand '(defo hotdog []
+  [s* ready "looks bad"]
+  [s halfeaten "regrets"]
+  [t eat ready > halfeaten [full]]))
+
+hotdog
