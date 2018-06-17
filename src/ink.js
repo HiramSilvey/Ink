9c3ac592239e36c3bf0fbac89ad7c9c42b901fe5
Hiram Silvey <blujay270@gmail.com>
2018-06-16 21:11:33 -0400

    updated item to use maps and include new attributes, added default scope function and scope class, added initial applyAction function

--- src/ink.js
+++ src/ink.js
@@ -6,8 +6,7 @@
  */
 
 // States represent the current status of an item
-function State(descriptor, subtext) {
-  this.descriptor = descriptor;
+function State(subtext) {
   this.subtext = subtext;
   this.transitions = {};
 
@@ -27,6 +26,14 @@ function Transition(nextState) {
   };
 }
 
+// Scopes are functions that dynamically determine which items are affected by an event action
+function Scope() {
+  // default: all neighboring items (same parent item)
+  this.default = function(item) {
+    return item.parentItem.subItems;
+  };
+}
+
 // Events apply actions to items defined by the scope
 function Event(action, scope) {
   this.action = action;
@@ -35,12 +42,38 @@ function Event(action, scope) {
 
 // Items are every contained piece of information
 // Example items: protagonist, sword, box, lightswitch, room, house
-function Item(descriptor, startState) {
-  this.descriptor = descriptor;
-  this.currentState = startState;
-  this.subitems = [];
+function Item() {
+  this.states = {};
+  this.currentState = null;
+  this.parentItem = null;
+  this.subItems = {};
 
-  this.addSubItem = function(item) {
-    this.subitems.push(item);
+  this.addState = function(descriptor, state, start=false) {
+    this.states[descriptor] = state;
+    if (start) { this.currentState = state; }
   };
+
+  this.addSubItem = function(descriptor, item) {
+    item.parentItem = this;
+    if (this.subitems.hasOwnProperty(descriptor)) {
+      this.subitems[descriptor].push(item);
+    } else {
+      this.subitems[descriptor] = [item];
+    }
+  };
+}
+
+// Applies an action to an item, triggering events recursively
+// Item states are updated before recursing, simulating the order dictated by time
+function applyAction(action, item) {
+  let transition = item.currentState.transitions[action];
+  item.currentState = transition.nextState;
+  for (var event of transition.events) {
+    let effectedItems = event.scope(item);
+    for (var effectedItemTypes of Object.values(effectedItems)) {
+      for (var effectedItem of effectedItemTypes) {
+        applyAction(event.action, effectedItem);
+      }
+    }
+  }
 }
