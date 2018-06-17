61a50ea6eec3c0c1243d05a6ad53e43281c3d78b
Hiram Silvey <blujay270@gmail.com>
2018-06-16 22:51:23 -0400

    cleaned up example code in ink.js

--- src/ink.js
+++ src/ink.js
@@ -7,88 +7,87 @@
 
 // States represent the current status of an item
 function State(subtext) {
-    this.subtext = subtext;
-    this.transitions = {};
+  this.subtext = subtext;
+  this.transitions = {};
 
-    this.addTransition = function(action, transition) {
-	this.transitions[action] = transition;
-    };
+  this.addTransition = function(action, transition) {
+    this.transitions[action] = transition;
+  };
 }
 
 // Transitions represent a change in state of an item
 // Events are applied when a transition occurs
 function Transition(nextState) {
-    this.nextState = nextState;
-    this.events = [];
+  this.nextState = nextState;
+  this.events = [];
 
-    this.addEvent = function(event) {
-	this.events.push(event);
-    };
+  this.addEvent = function(event) {
+    this.events.push(event);
+  };
 }
 
 // Scopes are functions that dynamically determine which items are affected by an event action
 let scopes = {
-    // default: all neighboring items (same parent item)
-    default: function(item) {
-	return item.parentItem.subItems;
-    }
+  // default: all neighboring items (same parent item)
+  default: function(item) {
+    return item.parentItem.subItems;
+  }
 }
 
 // Events apply actions to items defined by the scope
 function Event(action, scope) {
-    this.action = action;
-    this.scope = scope;
+  this.action = action;
+  this.scope = scope;
 }
 
 // Items are every contained piece of information
 // Example items: protagonist, sword, box, lightswitch, room, house
 function Item() {
-    this.states = {};
-    this.currentState = null;
-    this.parentItem = null;
-    this.subItems = {};
+  this.states = {};
+  this.currentState = null;
+  this.parentItem = null;
+  this.subItems = {};
 
-    this.addState = function(descriptor, state, start) {
-	this.states[descriptor] = state;
-	if (start) { this.currentState = state; }
-    };
+  this.addState = function(descriptor, state, start) {
+    this.states[descriptor] = state;
+    if (start) { this.currentState = state; }
+  };
 
-    this.addSubItem = function(descriptor, item) {
-	item.parentItem = this;
-	if (this.subItems.hasOwnProperty(descriptor)) {
-	    this.subItems[descriptor].push(item);
-	} else {
-	    this.subItems[descriptor] = [item];
-	}
-    };
-
-    
-    this.toString = function(){
-	var ans = "";
-	if(this.currentState) ans += "state: " + this.currentState.subtext + "\n";
-	for(var t in this.subItems){
-	    for(var i in this.subItems[t]){
-		ans += t + ": " + this.subItems[t][i].currentState.subtext + "\n";
-	    }
-	}
-	return ans;
+  this.addSubItem = function(descriptor, item) {
+    item.parentItem = this;
+    if (this.subItems.hasOwnProperty(descriptor)) {
+      this.subItems[descriptor].push(item);
+    } else {
+      this.subItems[descriptor] = [item];
     }
+  };
+
+  this.toString = function() {
+    let ans = "";
+    if (this.currentState) ans += "state: " + this.currentState.subtext + "\n";
+    for(var descriptor in this.subItems){
+      for(var subItem of this.subItems[descriptor]){
+        ans += descriptor + ": " + subItem.currentState.subtext + "\n";
+      }
+    }
+    return ans;
+  };
 }
 
 // Applies an action to an item, triggering events recursively
 // Item states are updated before recursing, simulating the order dictated by time
 function applyAction(action, item) {
-    let transition = item.currentState.transitions[action];
-    if(!transition) return;
-    item.currentState = transition.nextState;
-    for (var event of transition.events) {
-	let affectedItems = event.scope(item);
-	for (var t in affectedItems) {
-	    for(var i in affectedItems[t]){
-		applyAction(event.action, affectedItems[t][i]);
-	    }
-	}
+  let transition = item.currentState.transitions[action];
+  if (!transition) return;
+  item.currentState = transition.nextState;
+  for (var event of transition.events) {
+    let affectedItems = event.scope(item);
+    for (var affectedItemTypes of Object.values(affectedItems)) {
+      for(var affectedItem of affectedItemTypes){
+        applyAction(event.action, affectedItem);
+      }
     }
+  }
 }
 
 // example instantiation
@@ -113,7 +112,7 @@ hotdog.addState("half", half);
 model.addSubItem("protagonist", protagonist);
 model.addSubItem("hotdog", hotdog);
 
-console.log("BEFORE")
+console.log("BEFORE");
 console.log(model.toString());
 
 applyAction("eat",hotdog);
