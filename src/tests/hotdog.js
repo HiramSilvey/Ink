c0b738083c702e722a12a24f7bb5693004497d8b
Hiram Silvey <blujay270@gmail.com>
2018-06-24 21:50:34 -0400

    added back tests

---
+++ src/tests/hotdog.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+eval(fs.readFileSync('../ink.js')+'');
+eval(fs.readFileSync('../sdl.js')+'');
+
+let model = json2obj(sdl.parse(`
+protagonist {
+  awake*: "Basically the best";
+  asleep: "Zzz...";
+  |
+  fill: awake > asleep;
+};
+
+hotdog {
+  whole*: "a nice stick of processed meat";
+  halved: "nomnom";
+  |
+  eat: whole > halved [fill];
+}`));
+
+hotdog = model.subItems["hotdog"][0];
+//console.log(hotdog);
+
+console.log("BEFORE");
+console.log(model.toString());
+applyAction("eat",hotdog);
+console.log("AFTER");
+console.log(model.toString());
